5f451a4c230d72e37b3995e1fe72f1bf11facc38
hasolkim2007-glitch <hasolkim2007@gmail.com>
2026-09-15 18:52:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ class FirstComeLineView(discord.ui.View):
         await interaction.followup.send(f"✅ **[ {selection_name} ] 리롤 신청에 성공하셨습니다.**", ephemeral=True)
 
         # 운영진 로그 채널 전송
-        if LOG_CHANNEL_ID != 1549301300053811290 and interaction.guild:
+        if LOG_CHANNEL_ID != 0 and interaction.guild:
             try:
                 log_channel = await interaction.client.fetch_channel(int(LOG_CHANNEL_ID))
                 if log_channel:
