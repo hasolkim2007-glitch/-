b6b306aca619abdd067627bfa600e8bd038e9a23
hasolkim2007-glitch <hasolkim2007@gmail.com>
2026-09-15 17:44:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import datetime
-import flask
+from flask import flask
 import asyncio
 
 # ==========================================
